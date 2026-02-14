6c4f2fa1a210cbb4da07f3ddddff05745ea1cbf5
bt-144p <gug321gug@gmail.com>
2026-02-14 16:17:23 +0700

    Store categoryRule from OpenAI into DB

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 import { categorizeMockup } from 'src/calendar/mocks';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { classifyEventCategories } from './calendar.classifier';
 import { generateTask } from './task.generate';
 
 @Injectable()
 export class OpenAIService {
-  constructor(private readonly openai: OpenAI) {}
+  constructor(
+    private readonly openai: OpenAI,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async generatePlan(prompt: string) {
     return await generateTask({
@@ -16,9 +20,31 @@ export class OpenAIService {
   }
 
   async classifyRules() {
-    return await classifyEventCategories({
+    const classifiedEventCategories = await classifyEventCategories({
       client: this.openai,
       summaries: categorizeMockup.eventSummary,
     });
+    const categoryRules = classifiedEventCategories.outputFormat.results;
+
+    return await Promise.all(
+      categoryRules.map(({ keyword, category, tags }) => {
+        const tagsCreate = tags?.length ? tags.map((tag) => ({ tag })) : [];
+
+        return this.prisma.categoryRule.upsert({
+          where: { keyword },
+          create: {
+            keyword,
+            category,
+            tags: { create: tagsCreate },
+          },
+          update: {
+            tags: {
+              deleteMany: {},
+              create: tagsCreate,
+            },
+          },
+        });
+      }),
+    );
   }
 }
